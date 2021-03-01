c8a1293d303898992133ff3cac44e8bdc50bd219
Jacci Horvath <jaccinhorvath@gmail.com>
2021-03-01 13:48:07 -0500

    fixing to receive cached items

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,79 +10,63 @@ const FILES_TO_CACHE = [
     'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
   ];
   
-  const STATIC_CACHE = "static-cache-v1";
-  const RUNTIME_CACHE = "runtime-cache";
+  const CACHE_NAME = "static-cache-v2";
+  const DATA_CACHE_NAME = "data-cache-v1";
   
-  self.addEventListener("install", event => {
-    event.waitUntil(
-      caches
-        .open(STATIC_CACHE)
-        .then(cache => cache.addAll(FILES_TO_CACHE))
-        .then(() => self.skipWaiting())
-    );
-  });
-  
-  // The activate handler takes care of cleaning up old caches.
-  self.addEventListener("activate", event => {
-    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
-    event.waitUntil(
-      caches
-        .keys()
-        .then(cacheNames => {
-          return cacheNames.filter(
-            cacheName => !currentCaches.includes(cacheName)
-          );
-        })
-        .then(cachesToDelete => {
-          return Promise.all(
-            cachesToDelete.map(cacheToDelete => {
-              return caches.delete(cacheToDelete);
-            })
-          );
-        })
-        .then(() => self.clients.claim())
-    );
-  });
-  
-  self.addEventListener("fetch", event => {
-    if (
-      event.request.method !== "GET" ||
-      !event.request.url.startsWith(self.location.origin)
-    ) {
-      event.respondWith(fetch(event.request));
-      return;
-    }
-  
-    // handle runtime GET requests for data from /api routes
-    if (event.request.url.includes("/api/images")) {
-      event.respondWith(
-        caches.open(RUNTIME_CACHE).then(cache => {
-          return fetch(event.request)
-            .then(response => {
-              cache.put(event.request, response.clone());
-              return response;
-            })
-            .catch(() => caches.match(event.request));
+// install
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+
+  self.skipWaiting();
+});
+
+// activate
+self.addEventListener("activate", function(evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
         })
       );
-      return;
-    }
-  
-    // use cache first for all other requests for performance
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-  
-        // request is not in cache. make network request and cache the response
-        return caches.open(RUNTIME_CACHE).then(cache => {
-          return fetch(event.request).then(response => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
+    })
+  );
+
+  self.clients.claim();
+});
+
+// fetch
+self.addEventListener("fetch", function(evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+            return cache.match(evt.request);
           });
-        });
-      })
+      }).catch(err => console.log(err))
     );
-  });
+
+    return;
+  }
+
+  evt.respondWith(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(evt.request).then(response => {
+        return response || fetch(evt.request);
+      });
+    })
+  );
+});
